7dc997eea744b5668fe8be511d96865b522984f1
Erika Vigliucci <evigliucci@gmail.com>
2020-06-25 18:27:41 -0400

    mongo connection

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
-
+const connection = process.env.MONGODB_URI || "mongodb://localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(connection, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
