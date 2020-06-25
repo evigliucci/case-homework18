d4d3fddae16c85823c905c1148b4982e1add876d
Erika Vigliucci <evigliucci@gmail.com>
2020-06-25 18:17:16 -0400

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -23,4 +23,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(process.env.PORT || 5000);
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
