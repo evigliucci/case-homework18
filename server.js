adf804b498e9a4805d97194313acad2ea5cd6e77
Erika Vigliucci <evigliucci@gmail.com>
2020-06-24 22:46:14 -0400

    update port for heroku bug fix

--- server.js
+++ server.js
@@ -23,6 +23,4 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen(process.env.PORT || 5000);
